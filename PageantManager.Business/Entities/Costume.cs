a79f9b12f3a80a26e27795a0153dd4ea52232beb
Darick Carpenter <carpenterd@byui.edu>
2017-09-14 19:29:51 -0600

    none

---
+++ PageantManager.Business/Entities/Costume.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace PageantManager.Business.Entities
+{
+    public class Costume
+    {
+        public int CostumeId { get; set; }
+		public int PageantId { get; set; }
+		public string Description { get; set; }
+        public List<GarmentType> GarmentTypes { get; set; }
+    }
+}
