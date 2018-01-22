040a9ce047b3b8661864f7eead94ddd6ec59fa65
darickc <darick_c@hotmail.com>
2018-01-22 16:51:52 -0700

    none

---
+++ PageantManager.Web/Controllers/Api/GarmentTypesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PageantManager.Web.Controllers.Api
+{
+  public class GarmentTypesController : Controller
+  {
+    // GET
+    public IActionResult Index()
+    {
+      return
+      View();
+    }
+  }
+}
