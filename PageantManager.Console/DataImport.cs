3cb5ac3f60461955c6612420e759df1d0dbc6c93
darickc <darick_c@hotmail.com>
2018-02-13 22:30:09 -0700

    none

--- PageantManager.Console/DataImport.cs
+++ PageantManager.Console/DataImport.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using PageantManager.Business.Entities;
 using PageantManager.Business.Enums;
 
@@ -12,10 +13,39 @@ namespace PageantManager.Console
     {
         private readonly PageantManagerContext _ctx;
         private const string Csv = "YW_Manti_Final2.csv";
+        private const string Length = "Length";
+        private const string Waist = "Waist";
+        private const string Bust = "Bust";
+        
+        private MeasurementType lengthMeasurementType;
+        private MeasurementType waistMeasurementType;
+        private MeasurementType bustmeMeasurementType;
 
         public DataImport(PageantManagerContext ctx)
         {
             _ctx = ctx;
+            
+//            lengthMeasurementType = new MeasurementType
+//            {
+//                Name = Length
+//            };
+//            waistMeasurementType = new MeasurementType
+//            {
+//                Name = Waist
+//            };
+//            bustmeMeasurementType = new MeasurementType
+//            {
+//                Name = Bust
+//            };
+//
+//            _ctx.MeasurementTypes.Add(lengthMeasurementType);
+//            _ctx.MeasurementTypes.Add(waistMeasurementType);
+//            _ctx.MeasurementTypes.Add(bustmeMeasurementType);
+
+            var measurementTypes = _ctx.MeasurementTypes.ToList();
+            lengthMeasurementType = measurementTypes.SingleOrDefault(m => m.Name == Length);
+            waistMeasurementType = measurementTypes.SingleOrDefault(m => m.Name == Waist);
+            bustmeMeasurementType = measurementTypes.SingleOrDefault(m => m.Name == Bust);
         }
 
         public async Task Import()
@@ -49,7 +79,102 @@ namespace PageantManager.Console
             foreach (var @group in groups)
             {
                 System.Console.WriteLine($"{@group.Key.Gender} {@group.Key.Name} {@group.Key.Scene} {@group.Count()}");
+                AddGroup(@group.Key.Scene, @group.Key.Gender, @group.Key.Name, @group.ToList());
             }
+
+            await _ctx.SaveChangesAsync();
+
+        }
+
+        private void AddGroup(string scene, Gender gender, string name, List<CsvData> data)
+        {
+            var costume = new Costume
+            {
+                Name = $"{scene} {name}",
+                Gender = gender,
+                CostumeGarments = new List<CostumeGarment>()
+            };
+
+            _ctx.Costumes.Add(costume);
+            
+            var garmentType = new GarmentType
+            {
+                Name = $"{scene} {name}",
+                GarmentMeasurementTypes = new List<GarmentMeasurementType>(),
+                Garments = new List<Garment>()
+            };
+
+            if (data.Any(d => d.BustMax > 0 && d.BustMin > 0))
+            {
+                garmentType.GarmentMeasurementTypes.Add(new GarmentMeasurementType
+                {
+                    MeasurementType = bustmeMeasurementType
+                });
+            }
+            
+            if (data.Any(d => d.WaistMin > 0 && d.WaistMax > 0))
+            {
+                garmentType.GarmentMeasurementTypes.Add(new GarmentMeasurementType
+                {
+                    MeasurementType = waistMeasurementType
+                });
+            }
+            
+            if (data.Any(d => d.LengthMin > 0 && d.LengthMax > 0))
+            {
+                garmentType.GarmentMeasurementTypes.Add(new GarmentMeasurementType
+                {
+                    MeasurementType = lengthMeasurementType
+                });
+            }
+
+            foreach (var csvData in data)
+            {
+                var garment = new Garment
+                {
+                    AddedDate = DateTime.Now,
+                    GarmentMeasurements = new List<GarmentMeasurement>()
+                };
+                if (garmentType.GarmentMeasurementTypes.Any(m => m.MeasurementType.Name == Bust))
+                {
+                    garment.GarmentMeasurements.Add(new GarmentMeasurement
+                    {
+                        Max = csvData.BustMax,
+                        Min = csvData.BustMin,
+                        MeasurementType = bustmeMeasurementType
+                    });
+                }
+                
+                if (garmentType.GarmentMeasurementTypes.Any(m => m.MeasurementType.Name == Waist))
+                {
+                    garment.GarmentMeasurements.Add(new GarmentMeasurement
+                    {
+                        Max = csvData.WaistMax,
+                        Min = csvData.WaistMin,
+                        MeasurementType = waistMeasurementType
+                    });
+                }
+                
+                if (garmentType.GarmentMeasurementTypes.Any(m => m.MeasurementType.Name == Length))
+                {
+                    garment.GarmentMeasurements.Add(new GarmentMeasurement
+                    {
+                        Max = csvData.LengthMax,
+                        Min = csvData.LengthMin,
+                        MeasurementType = lengthMeasurementType
+                    });
+                }
+                
+                garmentType.Garments.Add(garment);
+            }
+            
+            costume.CostumeGarments.Add(new CostumeGarment
+            {
+                GarmentType = garmentType
+            });
+            
+            
+            
         }
     }
 }
