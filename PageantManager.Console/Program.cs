be438811f2275916f1391ba6c9f1467e92370af7
darickc <darick_c@hotmail.com>
2018-02-13 21:06:36 -0700

    none

---
+++ PageantManager.Console/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PageantManager.Business.Entities;
+using PageantManager.Business.Utilities;
+
+namespace PageantManager.Console
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                //.AddUserSecrets<Program>();
+
+            IConfigurationRoot config = builder.Build();
+
+            var services = new ServiceCollection();
+            //services.AddDbContext<PageantManagerContext>(options => options.UseInMemoryDatabase("pm"));
+            services.AddDbContext<PageantManagerContext>(options =>
+                options.UseSqlite("Data Source=../PageantManager.Web/PageanManager.db"));
+            Configuration.Configure(services);
+            services.AddScoped<DataImport>();
+            
+            ServiceProvider provider = services.BuildServiceProvider();
+            
+            
+            var dataImport = provider.GetService<DataImport>();
+            await dataImport.Import();
+        }
+    }
+}
